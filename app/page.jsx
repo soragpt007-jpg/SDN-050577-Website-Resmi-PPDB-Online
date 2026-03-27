4f7180ec80121477cebbb269e0fc5fb8252fa26e
Fzrnst <soragpt007@gmail.com>
2026-03-27 12:32:45 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -221,7 +221,7 @@ function Beranda({ navigateTo }) {
             <div className="lg:w-1/2 relative">
               <div className="absolute -inset-4 bg-yellow-300 rounded-3xl transform rotate-3 opacity-30"></div>
               <img 
-                src="https://i.postimg.cc/7Jwg1smQ/IMG-20260117-WA0003(1).jpg" 
+                src="https://i.postimg.cc/1nx0X56N/IMG-20250802-111254.jpg" 
                 alt="Kegiatan Belajar Siswa" 
                 className="relative rounded-3xl shadow-2xl object-cover h-[400px] w-full border-4 border-white"
               />
